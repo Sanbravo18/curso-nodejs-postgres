ac1df77fafd8d8fee1765d5c67202ef86b5fe1c4
Daniel Bravo <daniel.brv@hotmail.cl>
2022-01-04 18:19:15 -0300

    Se agrega el primer modelo de sequelize

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,4 +1,5 @@
 const { Sequelize } = require('sequelize');
+const setupModels = require('../db/models');
 
 const { config } = require('./../config/config');
 
@@ -10,5 +11,9 @@ const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
   logging: true,
 });
+
+setupModels(sequelize);
+
+sequelize.sync();
 
 module.exports = sequelize;
