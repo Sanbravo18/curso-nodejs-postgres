a112927cbb45e5d6d84e33d7ced5737c9848302a
Daniel Bravo <daniel.brv@hotmail.cl>
2022-01-06 08:09:10 -0300

    Se agrega migraciones con npm script

--- db/migrations/20220105143304-create-user.js
+++ db/migrations/20220105143304-create-user.js
@@ -1,21 +1,13 @@
 'use strict';
 
+const { UserSchema, USER_TABLE} = require('./../models/user.model');
+
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface) => {
+    await queryInterface.createTable(USER_TABLE, UserSchema);
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface) => {
+    await queryInterface.drop(USER_TABLE);
   }
 };
